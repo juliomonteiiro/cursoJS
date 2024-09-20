a57ba3230b9b79e14cffacc59a9cc1519ee38e4b
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-20 11:23:05 -0300

    Add ex011

---
+++ cursoJS/aula12/ex011.js
@@ -0,0 +1,20 @@
+// var idade = 18
+
+//     if (idade < 18 ){
+//         console.log(`Você é menor de idade!`)
+//     } else {
+//         console.log(`Você é maior de idade!`)
+//     }
+
+var idade = 14
+
+    console.log(`Você tem ${idade}`)
+
+    if (idade < 16 ){
+        console.log(`Você não vota!`)
+    } else if (idade < 18 || idade > 65  ) {
+        console.log(`Seu voto é opcional!`)
+    } else {
+        console.log(`Seu voto é obrigatório`)
+    }
+
