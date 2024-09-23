d832e1435f0f52bbb80d37600ed7fb21d38cc1fd
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-23 17:55:14 -0300

    add aula017

---
+++ aula017/objeto01.js
@@ -0,0 +1,12 @@
+let amigo = {
+    nome: 'João',
+    sexo: 'M',
+    peso: 70.5,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
